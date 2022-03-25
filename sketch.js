9bbba54ee3c7e34ac7cf8300a58dc34612655c97
gleaning-dreamer <61070473+gleaning-dreamer@users.noreply.github.com>
2022-03-24 20:25:19 -0400

    Update sketch.js
    changed framerate; added "clear text"  button; added new bahar assets (chili pepper, ball, diss notes, and toy wand); added arrays so bahar makes commentary about how well you are buying his love

--- sketch.js
+++ sketch.js
@@ -1,8 +1,16 @@
 //at the top set up the things that you need
-//remember to declare, initize variableuse strict;
+//remember to declare, initize variableuse strict
 
-let newFont, sound, amplitude, nb, mb, hb, lb, tc1, tc2, blt, bdf, bw, baharFace, baharType; //tc, etc is text color 1 and 2
+let newFont, sound, amplitude, nb, mb, hb, lb, tc1, tc2, blt, bdf, bw, bis, baharFace, baharType; //tc, etc is text color 1 and 2
 
+//preparing for the arrays
+let babyText = '';
+let babyArray = undefined;
+let clearSayings = 1;
+
+
+
+//filling the arrays with bahars thoughts
 let baharLicks = [
   'wow, lickable',
   'licky',
@@ -14,25 +22,20 @@ let baharLicks = [
   'you have purchased my love',
   'more licky treats',
   'yummy',
-  'purr purr purr',
 ];
 
 let baharDislikes = [
   'nft means not for touching',
   'do not financialize bahar',
   'vectoral capitalism much',
-  'dislike',
+  'get your invisble hand off me',
   'hissing',
-  'billionaires shouldnt exist',
-  'nope nope nope',
+  'bite the invisible hand',
   'disappointed in u',
   'i used to live in zucotti park',
   'mad at u',
   'is nothing sacred',
   '*angry yowling*',
-  'have u learned nothing',
-  'wall street gives me rash',
-  'can we move away from wall street',
 ];
 
 let baharEats = [
@@ -43,11 +46,7 @@ let baharEats = [
   'makes my coat so shiny',
   'totally fine',
   'nice and full',
-  'could go for some more',
   'you have bought my respect',
-  'crunch crunch crunch',
-  'you bought the diet one didnt u',
-  'diet catfood is ok',
   'prefer licky treat',
   '*inhales food*',
 ];
@@ -68,8 +67,8 @@ let baharNip = [
 ];
 
 let baharWaits = [
-  'what are you going to do',
-  'are you going to feed me',
+  'what',
+  'feed me',
   'my love is for sale',
   'kinda hongry',
   'can u give me treats',
@@ -96,67 +95,115 @@ function preload() {
   blt = loadImage('assets/blt.png');
   bdf = loadImage('assets/bdf.png');
   bw = loadImage('assets/bw.png');
+  bis = loadImage('assets/baharmarket.png');
 }
 
+function drawBackground () {
+  background(140, 176, 110);
+}
 
 function setup() { //comes first and happens once
   let cnv = createCanvas(800, 800); //make iiit
-  background(140,176,110); //pretty
+  background(140,176,110); //pretty colors
   button = createButton('click for bahar sounds');
   button.position (650, 720);
   button.mousePressed(toggleSound);
-  sound.amp(0.2); 
+  sound.amp(0.2);
+  button = createButton(`click to clear bahar's comments`);
+  button.position (300, 640);
+  button.mousePressed(drawBackground);
+  sound.amp(0.2);  
 }
 
-
+//bahartext is an object for the texts !!
   let baharText = {
   x: 1,
   y: 1,
   type: 0,
-};
-
+  shade: 255,
+  totalSayings: 10,
+  size: 20
+  }
   
-function draw(){  //draw is always looping
+function draw(){//draw is always looping
+  print(mouseX,mouseY);
+  frameRate (1);
+  
   
   baharText.x = random(0, 800);
   baharText.y = random(300, 550);
+  let previousType = baharText.type
 
-  if (mouseX < 350 && mouseY < 350) {
+  //response is based on mouse location
+  //location determines which array will be used
+
+  if (mouseX > 350 && mouseY > 200) {
   baharFace = hb;
-  baharType = 1;  
+  baharText.type = 2;
+  babyArray = baharEats;
   }
-  else if (mouseX > 350 && mouseY > 350) {
-  baharFace = hb;
-  baharType = 2;
-  }
-  else if (mouseX < 350 && mouseY > 350) {
+  else if (mouseX < 250 && mouseY > 500) {
   baharFace = lb;
-  baharType = 3;
+  baharText.type = 3;
+  babyArray = baharLicks;
   }
-  else if (mouseX >250 && mouseY < 250) {
+  else if (mouseX >500 && mouseY < 300) {
   baharFace = mb;
-  baharType = 4;
+  baharText.type = 4;
+  babyArray = baharDislikes;
   }
+  else if (mouseX < 260 && mouseY < 260) {
+  baharFace = hb;
+  baharText.type = 1;  
+  babyArray = baharNip;
+  }
+
   else {
   baharFace = nb;
-  baharType = 0
-    
-  };
+  baharText.Type = 0;
+  babyArray = baharWaits;
+  }
   
-
+//the images
+    
+  push();
+  textSize(30);
+  text('financialize petting', 500, 100);
+  pop();
+//choosing which text
+  if (baharType === 0) {
+    babyText = ``;
+  }
+  else {
+    if (baharType !== previousType) { //somewhere in here make it so that it 
+      drawBackground();
+      babyText = random(babyArray);
+    }
+  }
   push();
   image(bw, 100, 100);
   image(bdf, 500, 500);
   image(blt, 100, 500);
+  image(bis, 550, 100);
+  noStroke();
+  fill(120,150,96);
   circle(400, 400, 255);
   image (baharFace, 300, 300);
-  textSize(30);
-  text('financialize petting', 500, 100);
   pop();
-  text('test', baharText.x, baharText.y);
+
+  
+  //starting context for number of sayings 
+  //condition that the number of sayings drawn should be 10
+  //increasing sayingsDrawn by 1
+  push();
+  textSize(baharText.size);
+  fill(baharText.shade);
+  text(babyText, baharText.x, baharText.y);
+  pop();
+  
   
 }
-  
+
 function toggleSound() {
   
   if (sound.isPlaying() ){
@@ -165,6 +212,8 @@ function toggleSound() {
     sound.loop();
   }
 }
+
+
 
 
 {}
